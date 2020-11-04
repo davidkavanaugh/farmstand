6daaa549df4c80bcabc61ac2856243712d14b5bc
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2020-11-03 18:15:12 -0800

    !feat: signIn with Username-password auth0

--- sign_in/urls.py
+++ sign_in/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = "sign_in"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("get-user", views.get_user)
 ]
--- sign_in/views.py
+++ sign_in/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import environ
+import requests
 
 
 def index(request):
     return render(request, "sign-in.html")
+
+
+def get_user(request):
+    env = environ.Env()
+    auth_payload = {
+        'grant_type': 'password',
+        'client_id': env("AUTH0_CLIENT_ID"),
+        'username': request.POST['email'],
+        'password': request.POST['password']
+    }
+    headers = {'content-type': "application/x-www-form-urlencoded"}
+    auth0_token = requests.post(
+        env("AUTH0_SIGNIN_DOMAIN"), auth_payload, headers)
+    res = auth0_token
+    print(res)
+    return HttpResponse(res)
