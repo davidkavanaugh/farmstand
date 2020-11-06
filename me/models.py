aa485198cd98b931b7f67d4ca864aca1c174c30f
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2020-11-06 07:37:57 -0800

    feat: product model

--- me/models.py
+++ me/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from sign_up.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    unit = models.CharField(max_length=255)
+    farmer = models.OneToOneField(
+        User, related_name="farmer", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
