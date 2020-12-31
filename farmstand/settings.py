dd9da2798fbdde304fa61aafd544ce559074fe12
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2020-12-31 12:07:43 -0800

    settings

--- farmstand/settings.py
+++ farmstand/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = 'staticfiles'
+TEMPLATE_PATH = os.path.join(BASE_DIR, "templates")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
